50e320e9c3b2a46a13ae1db3ea85f22d0fc9e8e6
MattGuy00 <mattguy58@gmail.com>
2025-01-21 20:37:19 +1100

    small stuff

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include "Bios.h"
 #include "Bus.h"
 #include "Instruction.h"
+#include "Memory.h"
 
 #include <iostream>
 #include <bitset>
 
 int main() {
 	Bios bios { "scph1001.bin" };   
-	Bus bus { bios };
+	Memory memory {};
+	Bus bus { bios, memory };
 
-	Instruction instr { bios.fetch_32(0)};
+	memory.write_data(bios.first_64k());
+	Instruction instr { memory.read_data(0) };
 	std::cout << std::hex << instr << '\n';
 	
 	std::cout << std::bitset<32>{bios.fetch_32(0)} << '\n';
