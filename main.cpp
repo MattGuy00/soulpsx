ee3525c239c589af368431c45272bf938fd6ca47
MattGuy00 <mattguy58@gmail.com>
2025-01-25 19:58:25 +1100

    Nothing important

--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
 #include "Bios.h"
 #include "Bus.h"
-#include "Instruction.h"
 #include "Memory.h"
+#include "Cpu.h"
 
-#include <iostream>
-#include <bitset>
 
 int main() {
 	Bios bios { "scph1001.bin" };   
 	Memory memory {};
 	Bus bus { bios, memory };
 
-	memory.write_data(bios.first_64k());
-	Instruction instr { memory.read_data(0) };
-	std::cout << std::hex << instr << '\n';
-	
-	std::cout << std::bitset<32>{bios.fetch_32(0)} << '\n';
+	Cpu cpu { bus };
 
+	memory.write_data(bios.first_64k());
+	cpu.fetch_decode_execute();
+	
 	return 0;
 }
