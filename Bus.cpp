87ffa50b0e6f995b5ef9044e5cf71b25a792e8cb
MattGuy00 <mattguy58@gmail.com>
2025-02-01 15:02:10 +1100

    Translate virtual addresses into physical ones. Now able to read from ram or bios depending on the address.

--- Bus.cpp
+++ Bus.cpp
@@ -1,8 +1,24 @@
 #include "Bus.h"
+#include <cstdint>
 
 std::span<const std::byte> Bus::read_memory(uint32_t offset, uint32_t bytes = 0) {
-	if (offset >= bios.memory_region() && offset < bios.memory_region() + bios.rom_size()) {
-		return bios.read(offset - bios.memory_region(), bytes);
+	uint32_t kseg { offset & 0xf0000000 };
+	uint32_t physical_address { offset };
+	if (kseg >= 0x80000000 && kseg <= 0x90000000) {
+		// Shave off leftmost bit
+		physical_address &= 0x7fffffff;
+	} else if (kseg >= 0xa0000000 && kseg <= 0xb0000000) {
+		// shave off leftmost 3 bits
+		physical_address &= 0x1fffffff;
 	}
 
+	if (physical_address >= bios_memory_begin && physical_address < bios_memory_end) {
+		return bios.read(physical_address - bios_memory_begin, bytes);
+	} else if (physical_address >= ram_begin && physical_address < ram_end) {
+		return memory.read_data(4, physical_address);
+	}
+
+	std::cout << "Out of bounds access\n";
+
+	return std::span<const std::byte>();
 }
--- Bus.h
+++ Bus.h
@@ -6,9 +6,18 @@
 #include <cstdint>
 #include <span>
 
+
 struct Bus {
+	std::span<const std::byte> read_memory(uint32_t offset, uint32_t bytes);
+
 	Bios& bios;
 	Memory& memory;
 
-	std::span<const std::byte> read_memory(uint32_t offset, uint32_t bytes);
+	static constexpr uint32_t bios_memory_begin { 0x1fc00000 };
+	static constexpr uint32_t bios_size { 512 * 1024 };
+	static constexpr uint32_t bios_memory_end { bios_memory_begin + bios_size };
+
+	static constexpr uint32_t ram_begin { 0x0 };
+	static constexpr uint32_t ram_size { 2048 * 1024 };
+	static constexpr uint32_t ram_end { ram_begin + ram_size };
 };
