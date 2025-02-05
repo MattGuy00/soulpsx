d244b97c509299f78df48d8c8d4d2ffe39a4ebe5
MattGuy00 <mattguy58@gmail.com>
2025-02-05 21:08:25 +1100

    Able to write to memory when possible. Virtual addresses are now converted to their physical counterpart.

--- Bus.cpp
+++ Bus.cpp
@@ -1,9 +1,36 @@
 #include "Bus.h"
-#include <cstdint>
 
-std::span<const std::byte> Bus::read_memory(uint32_t offset, uint32_t bytes = 0) {
-	uint32_t kseg { offset & 0xf0000000 };
-	uint32_t physical_address { offset };
+#include <cstdint>
+#include <cstdlib>
+
+std::span<const std::byte> Bus::read_memory(uint32_t address, uint32_t bytes = 0) {
+	uint32_t physical_address { to_physical_address(address) };
+
+	if (physical_address >= bios_memory_begin && physical_address < bios_memory_end) {
+		return bios.read(physical_address - bios_memory_begin, bytes);
+	} else if (physical_address >= ram_begin && physical_address < ram_end) {
+		return ram.read(physical_address - ram_begin, bytes);
+	}
+
+	std::cout << "Out of bounds access\n";
+
+	return std::span<const std::byte>();
+}
+
+void Bus::write_memory(uint32_t address, std::span<const std::byte> data) {
+	uint32_t physical_address { to_physical_address(address) };
+
+	if (physical_address >= bios_memory_begin && physical_address < bios_memory_end) {
+		std::cout << "Illegal write to Read Only Memory (Bios)\n";
+	} else if (physical_address >= ram_begin && physical_address < ram_end) {
+		ram.write(physical_address - ram_begin, data);
+	}
+}
+
+// Translates the virtual memory address into its physical memory address
+uint32_t Bus::to_physical_address(uint32_t virtual_address) {
+	uint32_t kseg { virtual_address & 0xf0000000 };
+	uint32_t physical_address { virtual_address };
 	if (kseg >= 0x80000000 && kseg <= 0x90000000) {
 		// Shave off leftmost bit
 		physical_address &= 0x7fffffff;
@@ -12,13 +39,5 @@ std::span<const std::byte> Bus::read_memory(uint32_t offset, uint32_t bytes = 0)
 		physical_address &= 0x1fffffff;
 	}
 
-	if (physical_address >= bios_memory_begin && physical_address < bios_memory_end) {
-		return bios.read(physical_address - bios_memory_begin, bytes);
-	} else if (physical_address >= ram_begin && physical_address < ram_end) {
-		return memory.read_data(4, physical_address);
-	}
-
-	std::cout << "Out of bounds access\n";
-
-	return std::span<const std::byte>();
+	return physical_address;
 }
--- Bus.h
+++ Bus.h
@@ -1,17 +1,19 @@
 #pragma once
 
 #include "Bios.h"
-#include "Memory.h"
+#include "Ram.h"
 
 #include <cstdint>
 #include <span>
 
 
 struct Bus {
-	std::span<const std::byte> read_memory(uint32_t offset, uint32_t bytes);
+	std::span<const std::byte> read_memory(uint32_t address, uint32_t bytes);
+	void write_memory(uint32_t address, std::span<const std::byte> data);
+	uint32_t to_physical_address(uint32_t virtual_address);
 
 	Bios& bios;
-	Memory& memory;
+	Ram& ram;
 
 	static constexpr uint32_t bios_memory_begin { 0x1fc00000 };
 	static constexpr uint32_t bios_size { 512 * 1024 };
