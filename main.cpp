06b4a434ef0393eaff9a1c7b7b2af6650e334ba2
MattGuy00 <mattguy58@gmail.com>
2025-02-05 21:13:53 +1100

    Uses correct methods

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include "Bios.h"
 #include "Bus.h"
-#include "Memory.h"
+#include "Ram.h"
 #include "Cpu.h"
 
 
 int main() {
 	Bios bios { "scph1001.bin" };   
-	Memory memory {};
+	Ram memory {};
 	Bus bus { bios, memory };
 
 	Cpu cpu { bus };
 
-	memory.write_data(bios.read(0, 64 * 1024));
+	memory.write(0x0, bios.read(0, 64 * 1024));
 	cpu.fetch_decode_execute();
 	
 	return 0;
