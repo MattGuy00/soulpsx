1daa177b8d5c183a8b755392ae50fb514922bfd2
MattGuy00 <mattguy58@gmail.com>
2025-02-07 20:09:26 +1100

    Didn't realise you weren't meant to manually write 64k of BIOS to RAM

--- main.cpp
+++ main.cpp
@@ -3,15 +3,14 @@
 #include "Ram.h"
 #include "Cpu.h"
 
-
 int main() {
 	Bios bios { "scph1001.bin" };   
 	Ram memory {};
+
 	Bus bus { bios, memory };
 
 	Cpu cpu { bus };
 
-	memory.write(0x0, bios.read(0, 64 * 1024));
 	cpu.fetch_decode_execute();
 	
 	return 0;
