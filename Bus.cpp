ad033d82a80ab3afc599e4a3f0e922551a3e6b90
MattGuy00 <mattguy58@gmail.com>
2025-02-07 20:07:11 +1100

    Added region mask for a cleaner way of converting virtual addresses. Also added certain memory regions that aren't really important

--- Bus.cpp
+++ Bus.cpp
@@ -6,38 +6,54 @@
 std::span<const std::byte> Bus::read_memory(uint32_t address, uint32_t bytes = 0) {
 	uint32_t physical_address { to_physical_address(address) };
 
+	if (address % 4 != 0) {
+		std::cout << "Unaligned address exception (" << std::hex;
+		std::cout << physical_address << ")\n";
+	}
+
 	if (physical_address >= bios_memory_begin && physical_address < bios_memory_end) {
 		return bios.read(physical_address - bios_memory_begin, bytes);
 	} else if (physical_address >= ram_begin && physical_address < ram_end) {
 		return ram.read(physical_address - ram_begin, bytes);
+	} else if (physical_address >= io_ports_begin && physical_address < io_ports_end) {
+		return std::span{ io_ports }.subspan(physical_address - io_ports_begin, bytes);
+	} else if (physical_address >= cache_control_begin && physical_address < cache_control_end) {
+		std::cout << "cache control\n";
+	} else {
+		std::cout << "Read to unknown region\n";
+		std::exit(1);
 	}
-
-	std::cout << "Out of bounds access\n";
 
 	return std::span<const std::byte>();
 }
 
 void Bus::write_memory(uint32_t address, std::span<const std::byte> data) {
 	uint32_t physical_address { to_physical_address(address) };
+	
+	if (address % 4 != 0) {
+		std::cout << "Unaligned address exception (" << std::hex;
+		std::cout << physical_address << ")\n";
+	}
 
 	if (physical_address >= bios_memory_begin && physical_address < bios_memory_end) {
 		std::cout << "Illegal write to Read Only Memory (Bios)\n";
 	} else if (physical_address >= ram_begin && physical_address < ram_end) {
 		ram.write(physical_address - ram_begin, data);
+	} else if (physical_address >= io_ports_begin && physical_address < io_ports_end) {
+		std::copy(data.begin(), data.end(), io_ports.begin() + address - io_ports_begin);
+	} else if (physical_address >= cache_control_begin && physical_address < cache_control_end) {
+		std::cout << "Ignoring write to cache control\n";
+	} else if (physical_address >= expansion_region_2_begin && physical_address < expansion_region_2_end) { 
+		std::cout << "Ignoring write to expansion region 2\n";
+	} else {
+		std::cout << "Write to unknown region\n";
+		std::exit(1);
 	}
 }
 
 // Translates the virtual memory address into its physical memory address
 uint32_t Bus::to_physical_address(uint32_t virtual_address) {
-	uint32_t kseg { virtual_address & 0xf0000000 };
-	uint32_t physical_address { virtual_address };
-	if (kseg >= 0x80000000 && kseg <= 0x90000000) {
-		// Shave off leftmost bit
-		physical_address &= 0x7fffffff;
-	} else if (kseg >= 0xa0000000 && kseg <= 0xb0000000) {
-		// shave off leftmost 3 bits
-		physical_address &= 0x1fffffff;
-	}
-
+	uint32_t kseg { virtual_address >> 29 };
+	uint32_t physical_address { virtual_address & region_mask[kseg] };
 	return physical_address;
 }
--- Bus.h
+++ Bus.h
@@ -22,4 +22,31 @@ struct Bus {
 	static constexpr uint32_t ram_begin { 0x0 };
 	static constexpr uint32_t ram_size { 2048 * 1024 };
 	static constexpr uint32_t ram_end { ram_begin + ram_size };
+
+	static constexpr uint32_t io_ports_begin { 0x1f801000 };
+	static constexpr uint32_t io_ports_size { 4 * 1024 };
+	static constexpr uint32_t io_ports_end { io_ports_begin + io_ports_size };
+
+	static constexpr uint32_t cache_control_begin { 0xfffe0000 };
+	static constexpr uint32_t cache_control_size { 512 };
+	static constexpr uint32_t cache_control_end { cache_control_begin + cache_control_size };
+
+	static constexpr uint32_t expansion_region_2_begin { 0x1f802000 };
+	static constexpr uint32_t expansion_region_2_size { 8 * 1024 };
+	static constexpr uint32_t expansion_region_2_end { expansion_region_2_begin + expansion_region_2_size };
+
+
+	// Creating it here for now as its likely not used anywhere
+	std::array<std::byte, io_ports_size> io_ports {};
+
+	static constexpr std::array<uint32_t, 8> region_mask {
+		// KUSEG
+		0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
+		// KSEG0
+		0x7fffffff,
+		// KSEG1,
+		0x1fffffff,
+		// KSEG2
+		0xffffffff, 0xffffffff
+	};
 };
