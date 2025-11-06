5b68e0f69d7f6476419c02ab8f4be08430127f0e
mattguy <mattguy58@gmail.com>
2025-11-06 14:27:56 +1100

    Now uses our newly created memory maps and range class. Removed the Region enums.

--- Bus.cpp
+++ Bus.cpp
@@ -1,113 +1,116 @@
 #include "Bus.h"
 
-#include <cstdint>
-#include <cstdlib>
 #include <span>
+
+#include "Logger.h"
+#include "Memory.h"
 
 std::span<const std::byte> Bus::read_memory(uint32_t address, uint32_t bytes) const {
 	uint32_t physical_address { to_physical_address(address) };
-
-	if (physical_address == 0x1f8010f0) {
-		std::cout << "GPU send / recieve" << '\n';
-		std::exit(0);
+	if (Memory::Map::bios.contains(physical_address)){
+		return m_bios.read(Memory::Map::bios.offset(physical_address), bytes);
 	}
 
-	if (physical_address == 0x1f801814) {
-		std::cout << "GPU send / recieve" << '\n';
-		std::exit(0);
+	if (Memory::Map::ram.contains(physical_address)) {
+		return m_ram.read(Memory::Map::ram.offset(physical_address), bytes);
 	}
 
-	Region region { get_region(address) };
-	using enum Region;
-	switch (region) {
-		case bios: {
-			return m_bios.read(physical_address - bios_memory_begin, bytes);
-		}
-		case ram: {
-			return m_ram.read(physical_address - ram_begin, bytes);
-		}
-		case io_ports: {
-			std::cout << "I/O ports: " << std::hex << physical_address << '\n';
-			return std::span{ m_io_ports }.subspan(physical_address - io_ports_begin, bytes);
-		}
-		case cache_control: {
-			return {};
-		}
-		case expansion_1: {
-			return std::as_bytes(std::span{ &m_no_expansion, 1 });
-		}
-		case expansion_2: {
-			return {};
-		}
-		default: {
-			std::cout << "unknown region\n";
-			std::exit(1);
-		}
+	if (Memory::Map::gpu.contains(physical_address)) {
+		std::stringstream ss;
+		ss << "[BUS] Requesting GPU response at address (0x" << std::hex << physical_address << ")";
+		Logger::log(Logger::Level::warning, ss.str());
+		return m_gpu.get_response(physical_address);
 	}
-}
 
-std::span<const std::byte> Bus::read_memory(Region memory_region, uint32_t offset, uint32_t bytes) const {
-	using enum Region;
-	switch (memory_region) {
-		case bios: {
-			return m_bios.read(offset, bytes);
-		}
-		case ram: {
-			return m_ram.read(offset, bytes);
-		}
-		case io_ports: {
-			return std::span{ m_io_ports }.subspan(offset, bytes);
-		}
-		case cache_control: {
-			return {};
-		}
-		case expansion_1: {
-			return std::as_bytes(std::span{ &m_no_expansion, 1 });
-		}
-		case expansion_2: {
-			return {};
-		}
-		default: {
-			std::cout << "unknown region\n";
-			std::exit(1);
-		}
+	if (Memory::Map::irq_control.contains(physical_address)) {
+		std::stringstream ss;
+		ss << "[BUS] IRQ Control: reading 0x" << std::hex << physical_address;
+		Logger::log(Logger::Level::info, ss.str());
+		return std::as_bytes(std::span{ &dummy_variable, 1});
 	}
+
+	if (Memory::Map::timers.contains(physical_address)) {
+		std::stringstream ss;
+		ss << "[BUS] Timer: reading 0x" << std::hex << physical_address;
+		Logger::log(Logger::Level::info, ss.str());
+		return std::as_bytes(std::span{ &dummy_variable, 1});
+	}
+
+	if (Memory::Map::dma.contains(physical_address)) {
+		Logger::log(Logger::Level::info, "[BUS] Reading DMA.");
+		return std::as_bytes(std::span{ &dummy_variable, 1});
+	}
+
+	if (Memory::Map::cache_control.contains(physical_address)) {
+		return std::as_bytes(std::span{ &dummy_variable, 1});
+	}
+
+	if (Memory::Map::expansion_region_1.contains(physical_address)) {
+		return std::as_bytes(std::span{ &m_no_expansion, 1 });
+	}
+
+	if (Memory::Map::expansion_region_2.contains(physical_address)) {
+		return std::as_bytes(std::span{ &dummy_variable, 1});
+	}
+
+	if (Memory::Map::mem_control_1.contains(physical_address)) {
+		return std::as_bytes(std::span{ &dummy_variable, 1});
+	}
+
+	if (Memory::Map::mem_control_2.contains(physical_address)) {
+		return std::as_bytes(std::span{ &dummy_variable, 1});
+	}
+
+	if (Memory::Map::spu.contains(physical_address)) {
+		return std::as_bytes(std::span{ &dummy_variable, 1});
+	}
+
+	// If we have made it this far, then there is a read to an unknown area of memory
+	std::stringstream ss;
+	ss << "[BUS] Unknown read: " << std::hex << physical_address;
+	Logger::log(Logger::Level::error, ss.str());
+	std::exit(1);
 }
 
 void Bus::write_memory(uint32_t address, std::span<const std::byte> data) {
 	uint32_t physical_address { to_physical_address(address) };
+	if (Memory::Map::bios.contains(physical_address)){
+		Logger::log(Logger::Level::error, "[BUS] Illegal write to Read Only Memory");
+	} else if (Memory::Map::ram.contains(physical_address)) {
+		m_ram.write(Memory::Map::ram.offset(physical_address), data);
+	} else if (Memory::Map::gpu.contains(physical_address)) {
+		std::stringstream ss;
+		ss << "[BUS] Writing GPU address (0x" << std::hex << physical_address << ")";
+		uint32_t word {};
+		std::memcpy(&word, data.data(), sizeof(word));
+		ss << " Command: 0x" << word;
+		Logger::log(Logger::Level::warning, ss.str());
 
-	Region region { get_region(address) };
-	using enum Region;
-	switch (region) {
-		case bios: {
-			std::cout << "Illegal write to Read Only Memory (Bios)\n";
-			break;
-		}
-		case ram: {
-			m_ram.write(physical_address - ram_begin, data);
-			break;
-		}
-		case io_ports: {
-			//std::ranges::copy(data, m_io_ports.begin() + (address - io_ports_begin));
-			break;
-		}
-		case cache_control: {
-			std::cout << "Ignoring write to cache control\n";
-			break;
-		}
-		case expansion_1: {
-			std::cout << "Ignoring write to expansion region 1\n";
-			break;
-		}
-		case expansion_2: {
-			std::cout << "Ignoring write to expansion region 2\n";
-			break;
-		}
-		default: {
-			std::cout << "Write to unknown region " << "( " << std::hex << physical_address << ")\n";
-			std::exit(1);
-		}
+		m_gpu.receive_command(word);
+	} else if (Memory::Map::irq_control.contains(physical_address)) {
+		std::stringstream ss;
+		ss << "[BUS] IRQ Control: Ignoring write to 0x" << std::hex << physical_address;
+		Logger::log(Logger::Level::info, ss.str());
+	} else if (Memory::Map::timers.contains(physical_address)) {
+		std::stringstream ss;
+		ss << "[BUS] Timer: Ignoring write to 0x" << std::hex << physical_address;
+		Logger::log(Logger::Level::info, ss.str());
+	} else if (Memory::Map::dma.contains(physical_address)) {
+		Logger::log(Logger::Level::warning, "[BUS] Ignoring write to dma.");
+	} else if (Memory::Map::cache_control.contains(physical_address)) {
+		Logger::log(Logger::Level::warning, "[BUS] Ignoring write to cache control");
+	} else if (Memory::Map::expansion_region_1.contains(physical_address)) {
+		Logger::log(Logger::Level::warning, "[BUS] Ignoring write to expansion region 1");
+	} else if (Memory::Map::expansion_region_2.contains(physical_address)) {
+		Logger::log(Logger::Level::warning, "[BUS] Ignoring write to expansion region 2");
+	} else if (Memory::Map::mem_control_1.contains(physical_address)) {
+	} else if (Memory::Map::mem_control_2.contains(physical_address)) {
+	} else if (Memory::Map::spu.contains(physical_address)) {
+	} else {
+		std::stringstream ss;
+		ss << "Write to unknown memory region" << '(' << std::hex << physical_address << ')';
+		Logger::log(Logger::Level::error, ss.str());
+		std::exit(1);
 	}
 }
 
@@ -116,107 +119,4 @@ uint32_t Bus::to_physical_address(uint32_t virtual_address) {
 	uint32_t kseg { virtual_address >> 29 };
 	uint32_t physical_address { virtual_address & region_mask[kseg] };
 	return physical_address;
-}
-
-Region Bus::get_region(uint32_t virtual_address) const {
-	uint32_t physical_address { to_physical_address(virtual_address) };
-
-	if (physical_address >= bios_memory_begin && physical_address < bios_memory_end) {
-		return Region::bios;
-	}
-
-	if (physical_address >= ram_begin && physical_address < ram_end) {
-		return Region::ram;
-	}
-
-	if (physical_address >= io_ports_begin && physical_address < io_ports_end) {
-		return Region::io_ports;
-	}
-
-	if (physical_address >= cache_control_begin && physical_address < cache_control_end) {
-		return Region::cache_control;
-	}
-
-	if (physical_address >= expansion_region_1_begin && physical_address < expansion_region_1_end) {
-		return Region::expansion_1;
-	}
-
-	if (physical_address >= expansion_region_2_begin && physical_address < expansion_region_2_end) {
-		return Region::expansion_2;
-	}
-
-	return Region::unknown;
-}
-
-uint32_t Bus::get_region_start(Region region) {
-	using enum Region;
-	switch (region) {
-		case bios: return bios_memory_begin;
-		case ram: return ram_begin;
-		case io_ports: return io_ports_begin;
-		case cache_control: return cache_control_begin;
-		case expansion_1: return expansion_region_1_begin;
-		case expansion_2: return expansion_region_2_begin;
-		case unknown: return 0;
-	}
-
-	return 0;
-}
-
-uint32_t Bus::get_region_end(Region region) {
-	using enum Region;
-	switch (region) {
-		case bios: return bios_memory_end;
-		case ram: return ram_end;
-		case io_ports: return io_ports_end;
-		case cache_control: return cache_control_end;
-		case expansion_1: return expansion_region_1_end;
-		case expansion_2: return expansion_region_2_end;
-		case unknown: return 0;
-	}
-	return 0;
-}
-
-uint32_t Bus::get_relative_offset(uint32_t virtual_address) const {
-	uint32_t physical_address { to_physical_address(virtual_address) };
-	Region region { get_region(virtual_address) };
-	using enum Region;
-	switch (region) {
-		case bios: {
-			return physical_address - bios_memory_begin;
-		}
-		case ram: {
-			return physical_address - ram_begin;
-		}
-		case io_ports: {
-			return physical_address - io_ports_begin;
-		}
-		case cache_control: {
-			return physical_address - cache_control_begin;
-		}
-		case expansion_1: {
-			return physical_address - expansion_region_1_begin;
-		}
-		case expansion_2: {
-			return physical_address - expansion_region_2_begin;
-		}
-		case unknown: {
-			return virtual_address;
-		}
-
-	}
-	return virtual_address;
-}
-
-std::string_view Bus::region_name(Region region) {
-	using enum Region;
-	switch (region) {
-		case bios: return "bios";
-		case ram: return "ram";
-		case io_ports: return "io_ports";
-		case cache_control: return "cache_control";
-		case expansion_1: return "expansion_1";
-		case expansion_2: return "expansion_2";
-		default: return "unknown";
-	}
 }
--- Bus.h
+++ Bus.h
@@ -6,60 +6,20 @@
 #include <cstdint>
 #include <span>
 
-enum class Region {
-	bios,
-	ram,
-	io_ports,
-	cache_control,
-	expansion_1,
-	expansion_2,
-	unknown
-};
+#include "Gpu.h"
 
 struct Bus {
 	std::span<const std::byte> read_memory(uint32_t address, uint32_t bytes = 0) const;
-	std::span<const std::byte> read_memory(Region memory_region, uint32_t offset = 0, uint32_t bytes = 0) const;
 	void write_memory(uint32_t address, std::span<const std::byte> data);
 
 	static uint32_t to_physical_address(uint32_t virtual_address);
-	Region get_region(uint32_t virtual_address) const;
-	static uint32_t get_region_start(Region region);
-	static uint32_t get_region_end(Region region) ;
-	uint32_t get_relative_offset(uint32_t virtual_address) const;
-	static std::string_view region_name(Region region);
 
 	Bios& m_bios;
 	Ram& m_ram;
-
-	static constexpr uint32_t bios_memory_begin { 0x1fc00000 };
-	static constexpr uint32_t bios_size { 512 * 1024 };
-	static constexpr uint32_t bios_memory_end { bios_memory_begin + bios_size };
-
-	static constexpr uint32_t ram_begin { 0x0 };
-	static constexpr uint32_t ram_size { 2048 * 1024 };
-	static constexpr uint32_t ram_end { ram_begin + ram_size };
-
-	static constexpr uint32_t io_ports_begin { 0x1f801000 };
-	static constexpr uint32_t io_ports_size { 4 * 1024 };
-	static constexpr uint32_t io_ports_end { io_ports_begin + io_ports_size };
-
-	static constexpr uint32_t cache_control_begin { 0xfffe0000 };
-	static constexpr uint32_t cache_control_size { 512 };
-	static constexpr uint32_t cache_control_end { cache_control_begin + cache_control_size };
-
-	static constexpr uint32_t expansion_region_1_begin { 0x1f000000 };
-	static constexpr uint32_t expansion_region_1_size { 8 * 1024 };
-	static constexpr uint32_t expansion_region_1_end { expansion_region_1_begin + expansion_region_1_size };
+	Gpu& m_gpu;
 
 	static constexpr uint8_t m_no_expansion { 0xff };
-
-	static constexpr uint32_t expansion_region_2_begin { 0x1f802000 };
-	static constexpr uint32_t expansion_region_2_size { 8 * 1024 };
-	static constexpr uint32_t expansion_region_2_end { expansion_region_2_begin + expansion_region_2_size };
-
-
-	// Creating it here for now as its likely not used anywhere
-	std::array<std::byte, io_ports_size> m_io_ports {};
+	uint32_t dummy_variable {};
 
 	static constexpr std::array<uint32_t, 8> region_mask {
 		// KUSEG
