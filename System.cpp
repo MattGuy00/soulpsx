cea2d95c86e5e67d54aa512f880f322036e7881d
mattguy <mattguy58@gmail.com>
2025-11-06 14:35:58 +1100

    Added ability to pause the emulator.

--- System.cpp
+++ System.cpp
@@ -1,10 +1,13 @@
 #include "System.h"
 
 void System::run() {
-   if (m_pause_system) {
-      return;
-   }
+    if (m_pause_system) {
+        return;
+    }
+    m_cpu.fetch_decode_execute();
+}
 
-   m_cpu.fetch_decode_execute();
+void System::pause(bool pause_state) {
+    m_pause_system = pause_state;
 }
 
--- System.h
+++ System.h
@@ -2,10 +2,9 @@
 
 #include "Bios.h"
 
-#include <string_view>
-
 #include "Bus.h"
 #include "Cpu.h"
+#include "Gpu.h"
 #include "Ram.h"
 
 class System {
@@ -13,15 +12,17 @@ public:
 	const Cpu& get_cpu() const { return m_cpu; }
 	const Bios& get_bios() const { return m_bios; }
 	const Bus& get_bus() const { return m_bus; }
-	Region get_current_memory_region() const { return m_cpu.get_current_memory_region(); }
+	const Ram& get_ram() const { return m_memory; }
 
 	void run();
-	void pause(bool pause_state) { m_pause_system = pause_state; };
+	void pause(bool pause_state);
+	void quit(bool quit_state);
 private:
     static constexpr std::string bios_file_path { "../scph1001.bin" };
     Bios m_bios { bios_file_path };
     Ram m_memory {};
-    Bus m_bus { m_bios, m_memory };
+	Gpu m_gpu {};
+    Bus m_bus { m_bios, m_memory, m_gpu };
 	Cpu m_cpu { m_bus };
 	bool m_pause_system { false };
 };
