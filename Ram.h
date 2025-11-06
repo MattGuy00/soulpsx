4eee41b09cea014d422ecbb01497506649c67ae6
mattguy <mattguy58@gmail.com>
2025-11-06 14:35:08 +1100

    Added an easy way to fetch entire ram contents.

--- Ram.h
+++ Ram.h
@@ -9,7 +9,7 @@ public:
 
 	std::span<const std::byte> read(uint32_t address, uint32_t bytes);
 	void write(uint32_t address, std::span<const std::byte> data);
-
+	std::span<const std::byte> get_memory() const { return { m_ram.data(), m_ram_size }; }
 private:
 	static constexpr int m_ram_size { 2048 * 1024 };
 	std::array<std::byte, m_ram_size> m_ram {};
